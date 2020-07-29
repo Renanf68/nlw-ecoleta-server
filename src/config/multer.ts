9767db2c4001e524618afbb7fcd7bf17b16ea16a
RenanC <renancostam@gmail.com>
2020-07-28 22:49:09 -0300

    Upload to aws s3 ok

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,16 +1,46 @@
-import multer from 'multer';
-import path from 'path'
-import crypto from 'crypto'
+import multer from "multer";
+import path from "path";
+import crypto from "crypto";
+import aws from "aws-sdk";
+import multerS3 from "multer-s3";
+
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: path.resolve(__dirname, "..", "..", "uploads"),
+    filename(request, file, cb) {
+      const hash = crypto.randomBytes(6).toString("hex");
+      const filename = `${hash}-${file.originalname}`;
+      cb(null, filename);
+    },
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: process.env.AWS_BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
+      crypto.randomBytes(6, (err, hash) => {
+        if (err) cb(err);
+        const fileName = `${hash.toString("hex")}-${file.originalname}`;
+        const fullPath = "uploads/" + fileName;
+        cb(null, fullPath);
+      });
+    },
+  }),
+};
 
 export default {
-  storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
-    filename(request, file, callback) {
-      const hash = crypto.randomBytes(6).toString('hex');
-
-      const filename = `${hash}-${file.originalname}`
-
-      callback(null, filename)
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes[process.env.STORAGE_TYPE],
+  limits: {
+    fileSize: 2 * 1024 * 1024, //limite de 2Mb
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = ["image/jpeg", "image/pjpeg", "image/png"];
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type."));
     }
-  })
-}
+  },
+};
