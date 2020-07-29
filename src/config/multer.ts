47b2ab4c18c919a40e9be5a71999771d6108c3ab
RenanC <renancostam@gmail.com>
2020-07-29 16:10:59 -0300

    Test multer config fo local only

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,21 +1,21 @@
 import multer from "multer";
 import path from "path";
 import crypto from "crypto";
-import aws from "aws-sdk";
-import multerS3 from "multer-s3";
+//import aws from "aws-sdk";
+//import multerS3 from "multer-s3";
 
 const bucketName = process.env.AWS_BUCKET || "ecoletauploads/uploads";
 
 const storageTypes = {
   local: multer.diskStorage({
     destination: path.resolve(__dirname, "..", "..", "uploads"),
-    filename(request, file, cb) {
+    filename(req, file, cb) {
       const hash = crypto.randomBytes(6).toString("hex");
       const filename = `${hash}-${file.originalname}`;
       cb(null, filename);
     },
   }),
-  s3: multerS3({
+  /*s3: multerS3({
     s3: new aws.S3(),
     bucket: bucketName,
     contentType: multerS3.AUTO_CONTENT_TYPE,
@@ -28,13 +28,14 @@ const storageTypes = {
         cb(null, fileName);
       });
     },
-  }),
+  }),*/
 };
 
-const storageType =
+/*const storageType =
   process.env.STORAGE_TYPE === "local"
     ? storageTypes["local"]
-    : storageTypes["s3"];
+    : storageTypes["s3"];*/
+const storageType = storageTypes["local"];
 
 const config: multer.Options = {
   dest: path.resolve(__dirname, "..", "..", "uploads"),
