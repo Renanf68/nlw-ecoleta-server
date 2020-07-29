038b325493c48de59136da43df5472f5c14813a9
RenanC <renancostam@gmail.com>
2020-07-28 23:54:02 -0300

    Fix env vars on muler config

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -4,6 +4,8 @@ import crypto from "crypto";
 import aws from "aws-sdk";
 import multerS3 from "multer-s3";
 
+const bucketName = process.env.AWS_BUCKET || "ecoletauploads/uploads";
+
 const storageTypes = {
   local: multer.diskStorage({
     destination: path.resolve(__dirname, "..", "..", "uploads"),
@@ -15,7 +17,7 @@ const storageTypes = {
   }),
   s3: multerS3({
     s3: new aws.S3(),
-    bucket: "ecoletauploads/uploads", //process.env.AWS_BUCKET,
+    bucket: bucketName,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
     key: (req, file, cb) => {
@@ -29,9 +31,14 @@ const storageTypes = {
   }),
 };
 
+const storageType =
+  process.env.STORAGE_TYPE === "local"
+    ? storageTypes["local"]
+    : storageTypes["s3"];
+
 const config: multer.Options = {
   dest: path.resolve(__dirname, "..", "..", "uploads"),
-  storage: storageTypes["s3"], //process.env.STORAGE_TYPE
+  storage: storageType,
   limits: {
     fileSize: 2 * 1024 * 1024, //limite de 2Mb
   },
